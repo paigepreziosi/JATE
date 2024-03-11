2886692247d3e62b14971417fa519e462931940e
paigepreziosi <pvpreziosi@hotmail.com>
2024-03-11 00:03:18 -0400

    fix error

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,16 +26,18 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// Set up asset cache
 registerRoute(
-   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-   new StaleWhileRevalidate({
-     cacheName: 'asset-cache',
-     plugins: [
- 
-       new CacheableResponsePlugin({
-         statuses: [0, 200],
-       }),
-     ],
-   })
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
+
+// TODO: Implement asset caching
+// registerRoute();
